25755ad963e2b9a51318fae38dc194deedfaa84d
sonkom <smohamet1999@gmail.com>
2020-11-03 11:17:38 +0100

    main v1.0 done

--- MIPS_SONKO_CHANAL/Code/main.c
+++ MIPS_SONKO_CHANAL/Code/main.c
@@ -1,7 +1,55 @@
 #include "translation.h"
 #include "function.h"
 
-int main(int argc, char const *argv[]) {
-  printf("%x\n", translate("NOP"));
+#define LENLINE 20
+
+
+int main(int argc, char *argv[]) {
+
+  FILE *file_source, *file_result;
+  char *name_source, *name_result;
+  char character, read_line[LENLINE];
+  int index = 0;
+  int line_hexa;
+
+  if (argc < 3) printf("ERREUR : Arguments manquants\n");
+  else {
+
+    name_source = *(argv+1);
+    name_result = *(argv+2);
+
+    file_source = fopen(name_source, "r");
+    if(file_source == NULL) {
+      perror("Probleme d'ouverture fichier");
+      exit(1);
+    }
+
+    file_result = fopen(name_result, "w");
+    if(file_result == NULL) {
+      perror("Probleme d'ouverture fichier");
+      exit(1);
+    }
+
+    while (!feof(file_source)) {
+      fscanf(file_source, "%c", &character);
+
+      if(character != '\n'){
+        read_line[index] = character;
+        index++;
+      } else {
+        read_line[index] = '\0';
+        index = 0;
+
+        line_hexa = translate(read_line);
+
+        fprintf(file_result, "%x\n",line_hexa);
+      }
+    }
+  }
+
+  fclose(file_result);
+  fclose(file_source);
+
+
   return 0;
 }
